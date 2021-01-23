4e4053c6456841231ac4ce13c0e1ddae1ffd3e79
Raghav0499 <73594266+Raghav0499@users.noreply.github.com>
2021-01-23 15:24:39 +0530

    Installing ADDS Script
    PS script to install Active Directory Services

---
+++ DC.ps1
@@ -0,0 +1,16 @@
+﻿ param 
+   ( 
+        [Parameter(Mandatory)]
+        [String]$DomainName
+        )
+
+
+Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
+$pass="Passw0rd"
+$Pwd = $pass | ConvertTo-SecureString -AsPlainText -Force
+
+Install-ADDSForest -DomainName $DomainName -SafeModeAdministratorPassword $Pwd -Confirm:$false -Force -InstallDns:$true -DomainNetbiosName TEST -NoRebootOnCompletion
+Restart-computer
+
+#Install-ADDSForest -DomainName "test.com" -DomainNetBiosName "TEST" -InstallDns:$true -NoRebootCompletion:$true
+#Install-ADDSDomainController -DomainName "test.com" -InstallDns:$true -Credential (Get-Credential "test\administrator") 
